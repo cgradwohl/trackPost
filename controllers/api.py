1f0708b458e8f7e5c8398c391945e74ae4abc4b6
Luca de Alfaro <luca@dealfaro.org>
2016-10-20 13:17:50 -0700

    checkpoint, some mistake

---
+++ controllers/api.py
@@ -0,0 +1,24 @@
+import random
+
+# Mocks implementation.
+def get_tracks():
+    start_idx = int(request.vars.start_idx) if request.vars.start_idx is not None else 0
+    end_idx = int(request.vars.end_idx) if request.vars.end_idx is not None else 0
+    # We just generate a lot of of data.
+    tracks = []
+    for i in range(start_idx, end_idx):
+        t = dict(
+            artist = random.choice(['IU', 'Ailee', 'T-ara', 'Mamamoo']),
+            album = random.choice(['Modern times', 'Melting', 'Absolute']),
+            title = random.choice(['Falling U', 'TTL', 'Piano Man']),
+            duration = random.uniform(3 * 60, 4 * 60),
+            rating = random.randint(1, 5),
+            num_plays = random.randing(0, 100),
+        )
+    has_more = True
+    logged_in = False
+    return response.json(dict(
+        tracks=tracks,
+        logged_in=logged_in,
+        has_more=has_more,
+    ))
--- static/js/default_index.js
+++ static/js/default_index.js
@@ -6,6 +6,57 @@ var app = function() {
 
     Vue.config.silent = false; // show all warnings
 
+    // Extends an array
+    self.extend = function(a, b) {
+        for (var i = 0; i < b.length; i++) {
+            a.push(b[i]);
+        }
+    };
+
+    function get_tracks_url(start_idx, end_idx) {
+        var pp = {
+            start_idx: start_idx,
+            end_idx: end_idx
+        };
+        return "start/api/get_tracks" + "?" + $.param(pp);
+    }
+
+    self.get_tracks = function () {
+        $.getJSON(get_tracks_url(0, 20), function (data) {
+            self.vue.tracks = data.tracks;
+            self.vue.has_more = data.has_more;
+            self.vue.logged_in = data.logged_in;
+        })
+    };
+
+    self.get_more = function () {
+        var num_tracks = self.vue.papers.length;
+        $.getJSON(get_paper_url(num_tracks, num_tracks + 50), function (data) {
+            self.vue.has_more = data.has_more;
+            self.extend(self.vue.tracks, data.tracks);
+        });
+        self.vue.show_reviewers = false;
+    };
+
+    self.vue = new Vue({
+        el: "#vue-div",
+        delimiters: ['${', '}'],
+        unsafeDelimiters: ['!{', '}'],
+        data: {
+            tracks: [],
+            logged_in: false,
+            has_more: false
+        },
+        methods: {
+            get_more: self.get_more,
+        }
+
+    });
+
+    self.get_tracks();
+    $("#vue-div").show();
+
+
     return self;
 };
 
